4147a10ad8dd62f9d775e0ee39db8bc5a581fa55
xiaojueguan <xiaojueguan@gmail.com>
2020-01-13 17:57:34 +0800

    tmp

--- dawn/shell.py
+++ dawn/shell.py
@@ -17,19 +17,43 @@ def pull_images():
 
 class DawnArgumentParser(argparse.ArgumentParser):
     super(DawnArgumentParser, self).__init__(***args, **kwargs)
-    
+
     def error(self, message):
         return super().error(message)
-    
+
     def _get_option_tuples(self, option_string):
         return super()._get_option_tuples(option_string)
 
 class DawnShell(object):
     times = []
-    
+
     def __init__(self):
         self.client_logger = None
-        
+
+    def do_help(self, args):
+        pass
+
+    def _find_actions(self, subparsers, model, version, do_help):
+        for attr in [for attr in dir(model) if attr.startswith('do_')]:
+            command = attr.replace('_', '-')
+            callback = getattr(model, attr)
+            desc = callback.__doc__ or ''
+            action_help = desc.strip()
+            arguments = getattr(callback, 'arguments', [])
+            subparser = subparsers.add_parser(
+                command,
+                help=action_help,
+                description=desc,
+                add_help=False)
+            subparser.add_argment(
+                '-h', '--help',
+                action_help='help',
+                help=argparse.SUPPRESS)
+            self.subcommands[command] = subparser
+            for (args, kwargs) in arguments:
+                subparser.add_argument(*args, **kwargs)
+            subparser.set_defaults(func=callback)
+
     def get_base_parser(self, argv):
         parser = DawnArgumentParser(
             prog='dawn',
@@ -47,27 +71,54 @@ class DawnShell(object):
             '--debug',
             action='store_true',
             help='Print debugging output')
-        
-    def main(self, argv):
+        return parser
+
+    def get_subcommand_parser(self, do_help=False, argv):
+        parser = self.get_base_parser(argv)
+        self.subcommands = {}
+        subparsers = parser.add_subparsers(metavar='<subcommand>')
+        self._find_actions(subparsers, actions_module, version, do_help)
+        self._find_actions(subparsers, self, version, do_help)
+        for extension in self.extensions:
+            self._find_actions(subparsers, extension.module, version, do_help)
+        return parser
+
+    def do_bash_completion(self, _args):
+        commands = set()
+        options = set()
+        for sc_str, sc in self.subcommands.iterms():
+            commands.add(sc_str)
+            for option in sc._optionals._option_string_actions.keys():
+                options.add(option)
+        commands.remove('bash-completion')
+        commands.remove('bash-completion')
+        print(' '.join(commands | options))
+
+
+
+    def run(self, argv):
         parser = self.get_base_parser(argv)
         (args, args_list) = parser.parse_known_args(argv)
         subcommand_parser = self.get_subcommand_parser(
             do_help=do_help, argv=argv)
-        if args.help:
+        self.parser = subcommand_parser
+        if args.help or not argv:
             subcommand_parser.print_help()
             return 0
         args = subcommand_parser.parse_args(argv)
-        if args.func =
-        
-        
-        
+        if args.func = self.do_help:
+            self.do_help(args)
+            return 0
+        if args.func = self.do_bash_completion:
+            self.do_bash_completion(args)
+        args.func(args)
 
-        
+
 
 def main():
     try:
         argv = [for a in sys.argv[1:]]
-        DawnShell().main(argv)
+        DawnShell().run(argv)
     except Exception as  exc:
         print(exc)
     except KeyboardInterrupt:
@@ -157,4 +208,3 @@ def do_image_push():
 )
 def do_image_build():
     pass
-
