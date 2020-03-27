9d5a776b3a0935408b32e71ed170f3d066a507a3
xiaojueguan <xiaojueguan@gmail.com>
2020-03-27 18:12:04 +0800

    add nginx and ubuntu

---
+++ system/ubuntu/ubuntu18.sh
@@ -0,0 +1,31 @@
+# /etc/apt/sources.list
+
+# 替换默认的
+
+# http://archive.ubuntu.com/
+
+# 为
+
+# mirrors.aliyun.com
+
+
+# mv /etc/apt/sources.list /etc/apt/sources.list.bak
+
+cat << EOF > /etc/apt/sources.list
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+EOF
+
+apt update -y
